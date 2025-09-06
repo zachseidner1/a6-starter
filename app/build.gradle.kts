c4cc356668fc90585b690cb02cdee1daf0c248b0
zachseidner1 <zachary.seidner@gmail.com>
2025-09-05 21:41:01 -0400

    update target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.a6starter"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.a6starter"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
